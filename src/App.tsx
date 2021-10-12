39466d6bd4fcfc44303f7e96d7ef83cc579a9158
tahid02 <tanzim1463@gmail.com>
2021-10-12 14:33:38 +0600

    husky test two

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="App">hello all. hows going ?</div>
+  return <div className="App">hello all. hows going ?testing husky 2</div>
 }
 
 export default App;
